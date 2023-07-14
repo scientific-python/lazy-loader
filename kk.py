0265e1ea1a809623cc4ad72ca85642a5411ff7df
VNMabus <vnmabus@gmail.com>
2023-07-14 12:28:22 +0200

    Remove example file.

--- kk.py
+++
@@ -1,9 +0,0 @@
-import lazy_loader
-
-with lazy_loader.lazy_imports():
-    from . import rank
-    from ._fft_based import butterworth
-    from ._gabor import gabor, gabor_kernel
-    from ._gaussian import difference_of_gaussians, gaussian
-    import kk
-    import mm.ll

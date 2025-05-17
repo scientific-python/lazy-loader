ecb2325d0e96e092d69f19cc7a2f4450c87ba04f
Stefan van der Walt <stefanv@berkeley.edu>
2025-05-17 10:36:32 -0700

    Check for editable install before running tests

--- .spin/cmds.py
+++ .spin/cmds.py
@@ -1,4 +1,6 @@
+import importlib
 import sys
+import textwrap
 
 import click
 from spin.cmds.util import run
@@ -14,6 +16,20 @@ from spin.cmds.util import run
 )
 def test(pytest_args, coverage=False):
     """🔧 Run tests"""
+    if not importlib.util.find_spec("lazy_loader"):
+        click.secho(
+            textwrap.dedent("""\
+              ERROR: The package is not installed.
+
+              Please do an editable install:
+
+                pip install -e .[test]
+
+              prior to running the tests."""),
+            fg="red",
+        )
+        sys.exit(1)
+
     if coverage:
         pytest_args = ("--cov=lazy_loader", *pytest_args)
     run([sys.executable, "-m", "pytest", *list(pytest_args)])
